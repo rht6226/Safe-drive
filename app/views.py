2c190a1dac0eb6c1b20e97ece774f7d4fb5c9097
rht6226 <rohitraazanand567@gmail.com>
2020-10-23 14:35:41 +0530

    Deleted redundancy

--- app/views.py
+++ app/views.py
@@ -5,30 +5,9 @@ from flask import request
 from flask import jsonify
 from .drive import model_response
 
-
-
-# @app.route('/', methods=['GET'])
-# def test():
-#     return render_template('index.html')
-
-
-# @app.route("/predict", methods=["POST"])
-# def predict():
-#     message = request.get_json(force=True)
-#     encoded_image = message['image']
-#     response = model_response(model, encoded_image)
-#     return jsonify(response)
-
-
-# @app.route("/image", methods=["POST"])
-# def show():
-#     message = request.get_json(force=True)
-#     encoded_image = message['image']
-#     show_image(encoded_image)
-#     response = {
-#         'data': 'OKAY'
-#     }
-#     return jsonify(response)
+@app.route("/", methods=["GET"])
+def greet():
+    return "Drive Safe API is up and running"
 
 @app.route("/drive", methods=["POST"])
 def show():
